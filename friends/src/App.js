da22aeafb95db21e7ec15fa813a0602b4e844afe
Rockey Simmons <codenamerockey@gmail.com>
2019-09-17 13:57:56 -0700

    added formik and added the Private route file, Friendslist and axiosWithAuth file and linked them up

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import Private from './components/PrivateRoute';
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from App.Js</h1>
+      <div>
+        <Link to="/login">Login</Link>
+      </div>
+
+      <div>
+        <Link to="/friends_list">Friends List</Link>
+      </div>
+
+      <Route path="/login" component={Login} />
+      <Private path="/friends_list" component={FriendsList} />
     </div>
   );
 }
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendsList = () => {
+  return (
+    <div>
+      <h1>Hello from FriendsList</h1>
+    </div>
+  );
+};
+
+export default FriendsList;
