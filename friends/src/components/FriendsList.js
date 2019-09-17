e06aae2e3d95fbb749811174a1e4c3072c12f8f1
Rockey Simmons <codenamerockey@gmail.com>
2019-09-17 16:16:50 -0700

    added the friends list form and added functionality to add a friend through a post in the api

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import { axiosWithAuth } from './axiosWithAuth';
 
 const FriendsList = () => {
   return (
-    <div>
-      <h1>Hello from FriendsList</h1>
-    </div>
+    <Form className="App" style={{ width: 400 }}>
+      <div className="form-group">
+        <label className="label">Name:</label>
+        <Field className="input" name="name" type="text" autoComplete="off" />
+      </div>
+
+      <div className="form-group">
+        <label className="label">Age:</label>
+        <Field className="input" name="age" type="text" autoComplete="off" />
+      </div>
+
+      <div className="form-group">
+        <label className="label">Email:</label>
+        <Field className="input" name="email" type="email" autoComplete="off" />
+      </div>
+
+      <button className="btn" type="submit">
+        Add A Friend &rarr;
+      </button>
+    </Form>
   );
 };
 
-export default FriendsList;
+// formik needs to be called twice
+// the first time you give it and object
+// mapProps to Values returns the initial state
+export default withFormik({
+  mapPropsToValues() {
+    return {
+      name: '',
+      age: '',
+      email: ''
+    };
+  },
+
+  // we need to post the values from the form to the server, and set the token
+  // to local storage.
+  handleSubmit(values) {
+    // console.log(formikBag.props);
+
+    axiosWithAuth()
+      .post('/friends', values)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err.response.data);
+      });
+    // console.log(values);
+  }
+})(FriendsList);
